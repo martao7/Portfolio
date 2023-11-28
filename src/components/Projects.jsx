4387340486158f07bcf2d2b6e2d51d7a8070013d
Marta Organisciak <marta.organisciak@wp.pl>
2023-11-28 20:18:45 +0100

    new projects added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,12 +20,32 @@ export default function ImageGrid() {
       gitlink: "https://github.com/Phil-star1984/orbit_frontend",
       titel: "Orbit",
       description:
-        "My Final Project of WBS Coding School: An e-commerce store, displays the best- and the most played games with the best prices. ",
+        "My final Project of WBS Coding School: An e-commerce store, displays the best and the most played games.",
+    },
+    {
+      id: 3,
+      // src: "src/assets/icons/htmls.png",
+      src: "https://cdn.pixabay.com/photo/2016/04/24/04/53/globe-1348777_960_720.png",
+      link: "https://geo-game-11c6d3.netlify.app",
+      gitlink: "https://github.com/martao7/crossover-cityGame",
+      titel: "GeoGame",
+      description:
+        "Guess the names of the cities they are presented on the photos and win! At the end, you will see your total score and the comparison your score with the best results of other players.",
+    },
+    {
+      id: 4,
+      // src: "src/assets/icons/htmls.png",
+      src: "https://www.kindernetz.de/sendungen/schmecksplosion/1605000762801,image-knet-2538~_v-1x1@2dL_-029cdd853d61a51824ed2ee643deeae504b065c1.jpg",
+      link: "https://github.com/martao7/cookbook",
+      gitlink: "https://github.com/martao7/cookbook",
+      titel: "CookBook",
+      description:
+        "The website shows a static list of recipes. Each of recipe includes title, description, image, table with ingredients and preparation time.",
     },
   ];
   return (
     <>
-      <div className=" bg-gray-300 from-black to-gray-800 w-full h-full mt-20 mb-40 text-white md:h-screen text-center md:text-center">
+      <div className=" bg-gray-300 from-black to-gray-800 w-full h-full mt-20 mb-60 text-white md:h-screen text-center md:text-center">
         <div className="max-w-screen-lg p-14 mx-auto flex flex-col justify-center w-full h-full">
           <div className="pb-6">
             {/* <p className="py-80 "></p> */}
@@ -34,7 +54,7 @@ export default function ImageGrid() {
             </p>
 
             <p className="py-6 ">
-              the views on github and with netlify available:
+              the views available on GitHub and with Netlify:
             </p>
           </div>
 
@@ -55,15 +75,16 @@ export default function ImageGrid() {
                 <p className="w-60 max-h-full justify-center m-auto mb-8 ">
                   {description} <br></br>
                 </p>
+
                 <div className="flex items-center justify-center mb-4">
                   <button
-                    className="w-1/2 px-4 py-3 m-6 duration-200 hover:scale-105 bg-black"
+                    className="w-1/2 px-4 py-3 m-6 duration-200 rounded-md hover:scale-105 bg-black"
                     onClick={() => window.open(link, "_blank")}
                   >
                     View of Project
                   </button>
                   <button
-                    className="w-1/2 px-4 py-3 m-6 duration-200 hover:scale-105 bg-black"
+                    className="w-1/2 px-4 py-3 m-6 duration-200 rounded-md hover:scale-105 bg-black"
                     onClick={() => window.open(gitlink, "_blank")}
                   >
                     View of Code
@@ -72,6 +93,7 @@ export default function ImageGrid() {
               </div>
             ))}
           </div>
+
           {/* <p className="mb-20 mt-0"></p> */}
         </div>
       </div>
